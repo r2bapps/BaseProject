17379acda3302fd80c40156c80f9e446a1cc0cfd
R2B Apps <r2b.apps@gmail.com>
2014-06-13 19:10:17 +0200

    track clicklistener events on activity, added tracking constants, added anim resources

---
+++ BaseProject/test/r2b/apps/base/TestFrg2.java
@@ -0,0 +1,59 @@
+package r2b.apps.base;
+
+import r2b.apps.R;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class TestFrg2 extends BaseFragment {
+	
+	
+	
+	@Override
+	public void click(View view) {	
+		
+	}
+
+	@Override
+	protected int getLayout() {
+		return R.layout.support_frg2_test;
+	}
+
+	@Override
+	protected void initViews() {
+		
+	}
+
+	@Override
+	protected void initValues() {
+	}
+
+	@Override
+	protected void initListeners() {
+		
+	}
+
+	@Override
+	protected void init() {
+	}
+
+	@Override
+	protected void removeListeners() {
+	}
+
+	@Override
+	protected void close() {
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+	}
+	
+}
